a7138b0479a412dfb7e3976b4b46b12844328ece
SuperPox <calebkohl@gmail.com>
2021-01-19 17:18:35 -0700

    commit tree d29cefe9e1421c75c53c586051628b8d81c1441a

--- nested.rb
+++ nested.rb
@@ -15,9 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     temp = programmer_hash[:dennis_ritchie][:languages]
+     return temp.join
 end
 
 def adding_matz
@@ -63,6 +66,13 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+######################################################################################
+#yuki = {:yukihiro_matsumoto}
+#programmer_hash.merge!(yuki)
+#programmer_hash.merge![:yukihiro_matsumoto]  #[:known_for] << "Ruby"
+#programmer_hash.merge![:yukihiro_matsumoto][:languages] << "LISP", "C"
+
+
 
 	programmer_hash = 
  		{
@@ -79,8 +89,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+     programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+     #programmer_hash[:yukihiro_matsumoto][:languages] << "Javascript"
+     return programmer_hash
 end
 
 def changing_alan
@@ -101,7 +114,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      
 end
 
@@ -121,5 +135,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+          programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+          return programmer_hash
 end
